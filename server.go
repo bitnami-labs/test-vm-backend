24200621e5f542789398be3b370ef384888a6b1c
Jose Luis Vazquez Gonzalez <josvaz@vmware.com>
2020-09-04 06:27:15 +0200

    Use data driven API

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
 	"path"
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // VMServer is a http.Handler of VM REST requests
@@ -14,34 +15,66 @@ type VMServer struct {
 	address string
 }
 
+type serverHandler func(s *VMServer, w http.ResponseWriter, r *http.Request)
+
+type idHandlerFunc func(id int, w http.ResponseWriter, r *http.Request)
+
+// APISpec is the data source for API docs and mappings
+var APISpec = []struct {
+	method   string
+	path     string
+	bodySpec string
+	doc      string
+	handler  serverHandler
+}{
+	{
+		http.MethodGet, "/vms[/]?", "VMs JSON", "list All VMs",
+		func(s *VMServer, w http.ResponseWriter, r *http.Request) { s.list(w, r) },
+	},
+	{
+		http.MethodPut, "/vms/launch/\\d+", "", "launch VM by id",
+		func(s *VMServer, w http.ResponseWriter, r *http.Request) { s.requestIDfor(s.launch, w, r) },
+	},
+	{
+		http.MethodPut, "/vms/stop/\\d+", "", "stop a VM by id",
+		func(s *VMServer, w http.ResponseWriter, r *http.Request) { s.requestIDfor(s.stop, w, r) },
+	},
+	{
+		http.MethodGet, "/vms/\\d+", "VM JSON", "inspect a VM by id",
+		func(s *VMServer, w http.ResponseWriter, r *http.Request) { s.requestIDfor(s.inspect, w, r) },
+	},
+	{
+		http.MethodDelete, "/vms/\\d+", "VM JSON", "delete a VM by id",
+		func(s *VMServer, w http.ResponseWriter, r *http.Request) { s.requestIDfor(s.delete, w, r) },
+	},
+}
+
 // APIDoc dumps the API simple doc
 func (s *VMServer) APIDoc() string {
-	return `API
-GET /vms             -> VMs JSON          # list all VMs
-PUT /vms/launch/{id} -> Check status code # launch VM by id
-PUT /vms/stop/{id}   -> Check status code # a VM by id
-GET /vms/{id}        -> VM JSON           # inspect a VM by id
-DELETE /vms/{id}     -> Check status code # delete a VM by id`
+	var sb strings.Builder
+	sb.WriteString("API:\n")
+	for _, endpoint := range APISpec {
+		bodySpec := endpoint.bodySpec
+		if bodySpec == "" {
+			bodySpec = "Check status code"
+		}
+		sb.WriteString(fmt.Sprintf("%v\t%-20v\t-> %-20v\t# %v\n",
+			endpoint.method, endpoint.path, bodySpec, endpoint.doc))
+	}
+	return sb.String()
 }
 
 // ServeVM dispatchs the request to the correct method follwing the API schema
 func (s *VMServer) ServeVM(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("<- %v %v\n", r.Method, r.URL.Path)
-	switch {
-	case matches(r, http.MethodPut, "/vms/launch/\\d+"):
-		s.launch(w, r)
-	case matches(r, http.MethodPut, "/vms/stop/\\d+"):
-		s.stop(w, r)
-	case matches(r, http.MethodGet, "/vms/\\d+"):
-		s.inspect(w, r)
-	case matches(r, http.MethodDelete, "/vms/\\d+"):
-		s.delete(w, r)
-	case matches(r, http.MethodGet, "/vms[/]?"):
-		s.list(w, r)
-	default:
-		msg := fmt.Sprintf("%v %v not allowed", r.Method, r.URL.Path)
-		http.Error(w, msg, http.StatusMethodNotAllowed)
+	for _, endpoint := range APISpec {
+		if matches(r, endpoint.method, endpoint.path) {
+			endpoint.handler(s, w, r)
+			return
+		}
 	}
+	msg := fmt.Sprintf("%v %v not allowed", r.Method, r.URL.Path)
+	http.Error(w, msg, http.StatusMethodNotAllowed)
 }
 
 func matches(r *http.Request, method, pathRegex string) bool {
@@ -63,45 +96,31 @@ func (s *VMServer) list(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(s.vmm.List().String()))
 }
 
-func (s *VMServer) launch(w http.ResponseWriter, r *http.Request) {
+func (s *VMServer) requestIDfor(f idHandlerFunc, w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(path.Base(r.URL.Path))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	err = s.vmm.Launch(id)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
+	f(id, w, r)
 }
 
-func (s *VMServer) stop(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	err = s.vmm.Stop(id)
-	if err != nil {
+func (s *VMServer) launch(id int, w http.ResponseWriter, r *http.Request) {
+	if err := s.vmm.Launch(id); err != nil {
 		http.Error(w, err.Error(), http.StatusNotFound)
 	}
 }
 
-func (s *VMServer) delete(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
+func (s *VMServer) stop(id int, w http.ResponseWriter, r *http.Request) {
+	if err := s.vmm.Stop(id); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
 	}
+}
+
+func (s *VMServer) delete(id int, w http.ResponseWriter, r *http.Request) {
 	s.vmm.Delete(id)
 }
 
-func (s *VMServer) inspect(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+func (s *VMServer) inspect(id int, w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(s.vmm.Inspect(id).String()))
 }
