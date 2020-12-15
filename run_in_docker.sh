8b7506af0b7b50023d56ef6d3e8ca9782234b525
josvaz <josvaz@vmware.com>
2020-12-15 15:31:26 +0100

    Use port mapping instead of host network (#19)
    It is more standard and seems Mac and Windows don't support host
    networking so well anyway.

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -2,5 +2,5 @@
 # Copyright 2020 VMware, Inc.
 # SPDX-License-Identifier: BSD-2-Clause
 
-docker build . -t test-vmbackend && docker run --rm --network=host test-vmbackend ${@}
+docker build . -t test-vmbackend && docker run --rm -p 8080:8080 test-vmbackend ${@}
 
